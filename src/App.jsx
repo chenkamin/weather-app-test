4b1759868706939f7585be4a8730b0ec5b16aea1
chenkamin <chenkamin@gmail.com>
2023-01-16 10:19:01 +0200

    error handle and input typr check

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,40 @@
 import { useState,useRef } from 'react'
 import {TextField,Button, Stack} from '@mui/material/'
-
+import axios from 'axios'
 import './App.css'
 
+const API_KEY = 'dc350a4d62c5eda7e3adc31cb1df51b8'
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
 function App() {
   const textRef = useRef();
-  const API_KEY = 'dc350a4d62c5eda7e3adc31cb1df51b8'
   const [forecast, setForecast] = useState(null)
-
+  const [error, setError] = useState(null)
   async function handleSubmit(){
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${textRef.current.value}&appid=${API_KEY}`)
-    const data = await response.json()
-    setForecast(data)
-    console.log(data)
+    try{
+      setError(null)
+      const [fieldType , fieldValue] =  handleInputType();
+      const response = await axios.get(`${BASE_URL}?${fieldType}=${fieldValue}&appid=${API_KEY}`)
+      // const data = response.data
+      setForecast(response.data)
+      console.log(response.data)
+    }catch(error){
+      console.log(error)
+      setError(error)
+    }
   }
+
+  function handleInputType(){
+  let fieldType ;
+    const fieldValue = textRef.current.value
+    if(/\d/.test(fieldValue)){
+      console.log('zip')
+      fieldType= 'zip'
+    }else{
+      fieldType = 'q'
+    }
+    return [fieldType ,fieldValue] ;
+  }
+  
   return (
     <>
     <h1>Weather App</h1>
@@ -21,6 +42,7 @@ function App() {
     <TextField   label="Enter City or Zip Code" variant="outlined"  inputRef={textRef}  />
     <Button variant="outlined" onClick={handleSubmit}>Search</Button>
     </Stack>
+    {error && <p>Something went wrong</p>}
     </>
   )
 }
