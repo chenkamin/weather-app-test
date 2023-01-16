3f0a32ac74dcd5cda622a53929625f8844217054
chenkamin <chenkamin@gmail.com>
2023-01-16 10:08:26 +0200

    first api call check with handle submit and basic form

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState,useRef } from 'react'
+import {TextField,Button, Stack} from '@mui/material/'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const textRef = useRef();
+  const API_KEY = 'dc350a4d62c5eda7e3adc31cb1df51b8'
+  const [forecast, setForecast] = useState(null)
 
+  async function handleSubmit(){
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${textRef.current.value}&appid=${API_KEY}`)
+    const data = await response.json()
+    setForecast(data)
+    console.log(data)
+  }
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+    <h1>Weather App</h1>
+    <Stack direction="row" spacing={2}>
+    <TextField   label="Enter City or Zip Code" variant="outlined"  inputRef={textRef}  />
+    <Button variant="outlined" onClick={handleSubmit}>Search</Button>
+    </Stack>
+    </>
   )
 }
 
